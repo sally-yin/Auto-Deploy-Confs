ed0bd2a1b7c8813635887e2bbfa6b0f8474a7325
harold <harold.hao@tradesparq.com.cn>
2015-09-17 16:48:09 +0800

    first demo(for tradespider-client)

---
+++ auto-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#auto deploy tradespider client
+cd ../
+echo "delete old file"
+rm -rf tradespider/ glassesbag/ tradespider-docker/
+echo "pull from github"
+git clone git@github.com:Tradesparq/glassesbag.git -b pg
+git clone git@github.com:Tradesparq/tradespider.git
+git clone git@github.com:hgdharold/tradespider-docker.git
+
+echo "start project"
+cd tradespider-docker
+docker-compose stop && \
+echo y | docker-compose rm && \
+docker-compose up -d
+
+echo "success"
+~                 
